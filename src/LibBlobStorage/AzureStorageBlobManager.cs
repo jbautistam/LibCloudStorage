1ff33927ee0ec017540657b1f10a547a69434550
Jose Antonio Bautista Montejo <jbautistam@gmail.com>
2023-11-19 14:55:33 +0100

    Net 7

--- src/LibBlobStorage/AzureStorageBlobManager.cs
+++ src/LibBlobStorage/AzureStorageBlobManager.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Azure.Storage.Blobs.Specialized;
+
 using Bau.Libraries.LibBlobStorage.Metadata;
 
 namespace Bau.Libraries.LibBlobStorage
